52782f3ccc962883e2c04f60d88927daea44465e
vuoz <121556153+vuoz@users.noreply.github.com>
2025-12-30 16:27:01 +0100

    added config for my nrf52833 based zongle zmk dongle

---
+++ src/boards/zongle/board.h
@@ -0,0 +1,67 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2018 Ha Thach for Adafruit Industries
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef ZONGLE_H
+#define ZONGLE_H
+
+/*------------------------------------------------------------------*/
+/* CLOCK - 453-00059 does not have 32.76kHz crystal
+ *------------------------------------------------------------------*/
+#define BOARD_HAS_CRYSTAL_LFCLK 0
+
+
+/*------------------------------------------------------------------*/
+/* LED
+ *------------------------------------------------------------------*/
+#define LEDS_NUMBER     1
+#define LED_PRIMARY_PIN 25
+#define LED_STATE_ON    1
+
+/*------------------------------------------------------------------*/
+/* BUTTON
+ *------------------------------------------------------------------*/
+#define BUTTONS_NUMBER 2
+#define BUTTON_1       1
+#define BUTTON_2       2
+#define BUTTON_PULL    NRF_GPIO_PIN_PULLUP
+
+//--------------------------------------------------------------------+
+// BLE OTA
+//--------------------------------------------------------------------+
+#define BLEDIS_MANUFACTURER "Ezurio"
+#define BLEDIS_MODEL        "453-00059"
+
+//--------------------------------------------------------------------+
+// USB
+//--------------------------------------------------------------------+
+#define USB_DESC_VID          0x239A
+#define USB_DESC_UF2_PID      0x00D8
+#define USB_DESC_CDC_ONLY_PID 0x00D8
+
+#define UF2_PRODUCT_NAME      "Zongle"
+#define UF2_VOLUME_LABEL      "ZongleBOOT"
+#define UF2_BOARD_ID          "nRF52833-zongle-v0.0.5"
+#define UF2_INDEX_URL         "https://github.com/vuoz/zongle"
+
+#endif // ZONGLE_H
